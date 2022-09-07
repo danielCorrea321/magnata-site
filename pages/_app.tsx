b37789ce4c780d6ae6f17b899ba67ee02c92002a
Daniel Correa <daniel.correa321@hotmail.com>
2022-09-07 09:52:27 -0300

    header criado faltando ainda algumas modificiações

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [show, setShow] = useState('none')
+
+  return (
+  <>
+    <header>
+      <div>LOGO</div>
+      <ul>
+        <li>home</li>
+        <li>local</li>
+        <li>eventos</li>
+      </ul>
+      <div 
+        className="menuBar"
+        onClick={() => show === 'none' ? setShow('block') : setShow('none')}
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </header>
+
+    <div 
+      className='menuResp' 
+      style={{display: show}}
+    >
+      <ul>
+        <li>home</li>
+        <li>local</li>
+        <li>eventos</li>
+      </ul>
+    </div>
+    
+    <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
